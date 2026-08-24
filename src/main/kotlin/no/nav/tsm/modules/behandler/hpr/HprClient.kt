ef41244ab483291b129dcb41790322cd1eb26c22
Andreas <danduras@gmail.com>
2026-08-24 09:26:21 +0200

    chore: dont parse and save behandlere mid network

--- src/main/kotlin/no/nav/tsm/modules/behandler/hpr/HprClient.kt
+++ src/main/kotlin/no/nav/tsm/modules/behandler/hpr/HprClient.kt
@@ -4,9 +4,10 @@ import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.utils.io.CancellationException
 import io.ktor.utils.io.jvm.javaio.*
-import java.util.zip.ZipInputStream
 import kotlinx.coroutines.Dispatchers
+import java.util.zip.ZipInputStream
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import no.nav.tsm.core.Environment
@@ -32,42 +33,41 @@ class HprClient(
 
     fun getExport() =
         flow<Behandler> {
-                val token = texas.maskinporten(scope).token
-                httpClient
-                    .prepareGet(hprUrl) {
-                        bearerAuth(token)
-                    }
-                    .execute {
-                        if (it.status.isSuccess()) {
-                            ZipInputStream(it.bodyAsChannel().toInputStream()).use { zip ->
-                                try {
+            val token = texas.maskinporten(scope).token
+            val response =
+                httpClient.get(hprUrl) {
+                    bearerAuth(token)
+                }
 
-                                    while (zip.nextEntry != null) {
-                                        val jsonParser = jsonFactory.createParser(ObjectReadContext.empty(), zip)
-                                        while (jsonParser.nextToken() != null) {
-                                            if (jsonParser.currentToken().id() == JsonTokenId.ID_START_OBJECT) {
-                                                try {
-                                                    emit(
-                                                        behandlerObjectMapper.readValue(
-                                                            jsonParser,
-                                                            Behandler::class.java,
-                                                        )
-                                                    )
-                                                } catch (e: Exception) {
-                                                    logger.error("Error while parsing json", e)
-                                                }
-                                            }
-                                        }
-                                    }
-                                } catch (cause: Throwable) {
-                                    logger.error("Error while parsing offentlig hpr api", cause)
+            if (!response.status.isSuccess()) {
+                logger.error("Could not get export from hpr")
+                throw RuntimeException("Could not get export from hpr")
+            }
+
+            ZipInputStream(response.bodyAsChannel().toInputStream()).use { zip ->
+                try {
+                    while (zip.nextEntry != null) {
+                        val jsonParser = jsonFactory.createParser(ObjectReadContext.empty(), zip)
+                        while (jsonParser.nextToken() != null) {
+                            if (jsonParser.currentToken().id() == JsonTokenId.ID_START_OBJECT) {
+                                try {
+                                    emit(
+                                        behandlerObjectMapper.readValue(
+                                            jsonParser,
+                                            Behandler::class.java,
+                                        )
+                                    )
+                                } catch (e: Exception) {
+                                    logger.error("Error while parsing json", e)
                                 }
                             }
-                        } else {
-                            logger.error("Could not download data from hpr")
-                            throw RuntimeException("Could not download data from hpr")
                         }
                     }
+                } catch (e: CancellationException) {
+                    throw e
+                } catch (e: Exception) {
+                    logger.error("Error while parsing offentlig hpr api", e)
+                }
             }
-            .flowOn(Dispatchers.IO)
+        }.flowOn(Dispatchers.IO)
 }
